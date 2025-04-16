5f0303fb1e9c681f244dd8d413b69f9774ff2ebf
maheshworpaudel5001 <maheshworpaudel5001@gmail.com>
2025-04-16 16:25:23 -0400

    Moved the GRUmodel class block here.

---
+++ models.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+
+
+class GRUModel(nn.Module):
+    def __init__(
+        self, input_dim, hidden_dim, output_dim, num_layers, bidirectional, dropout
+    ):
+        super(GRUModel, self).__init__()
+        self.gru = nn.GRU(
+            input_dim,
+            hidden_dim,
+            num_layers=num_layers,
+            batch_first=True,
+            bidirectional=bidirectional,
+            dropout=dropout,
+        )
+        self.fc = nn.Linear(hidden_dim * (2 if bidirectional else 1), output_dim)
+
+    def forward(self, x):
+        out, hn = self.gru(x)
+        out = self.fc(out[:, -1, :])
+        out = torch.relu(out)
+        return out
